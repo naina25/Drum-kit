0ef31afb56a263e9ef43d99af99421833b7199dc
naina <nanu02d8@gmail.com>
2020-09-25 20:59:50 +0530

    added keyboard events

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
 document.querySelectorAll('.drum').forEach(drum => drum.addEventListener('click', function () {
-    if (this.innerHTML === "w") {
-        let audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-    }
-    if (this.innerHTML === "a") {
-        let audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-    }
-    if (this.innerHTML === "s") {
-        let audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-    }
-    if (this.innerHTML === "d") {
-        let audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-    }
-    if (this.innerHTML === "j") {
-        let audio = new Audio("sounds/snare.mp3");
-        audio.play();
-    }
-    if (this.innerHTML === "k") {
-        let audio = new Audio("sounds/crash.mp3");
-        audio.play();
-    }
-    if (this.innerHTML === "l") {
-        let audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
+    let keyinnerHTML = this.innerHTML;
+    switch (keyinnerHTML) {
+        case "w":
+            let audio1 = new Audio("sounds/tom-1.mp3");
+            audio1.play();
+            break;
+        case "a":
+            let audio2 = new Audio("sounds/tom-2.mp3");
+            audio2.play();
+            break;
+        case "s":
+            let audio3 = new Audio("sounds/tom-3.mp3");
+            audio3.play();
+            break;
+        case "d":
+            let audio4 = new Audio("sounds/tom-4.mp3");
+            audio4.play();
+            break;
+        case "j":
+            let audio5 = new Audio("sounds/snare.mp3");
+            audio5.play();
+            break;
+        case "k":
+            let audio6 = new Audio("sounds/crash.mp3");
+            audio6.play();
+            break;
+        case "l":
+            let audio7 = new Audio("sounds/kick-bass.mp3");
+            audio7.play();
+            break; 
+        default:
+            break;
     }
 }));
+
+document.addEventListener('keydown', function(event){
+    switch (event.key) {
+        case "w":
+            let audio1 = new Audio("sounds/tom-1.mp3");
+            audio1.play();
+            break;
+        case "a":
+            let audio2 = new Audio("sounds/tom-2.mp3");
+            audio2.play();
+            break;
+        case "s":
+            let audio3 = new Audio("sounds/tom-3.mp3");
+            audio3.play();
+            break;
+        case "d":
+            let audio4 = new Audio("sounds/tom-4.mp3");
+            audio4.play();
+            break;
+        case "j":
+            let audio5 = new Audio("sounds/snare.mp3");
+            audio5.play();
+            break;
+        case "k":
+            let audio6 = new Audio("sounds/crash.mp3");
+            audio6.play();
+            break;
+        case "l":
+            let audio7 = new Audio("sounds/kick-bass.mp3");
+            audio7.play();
+            break; 
+        default:
+            break;
+    }
+})
