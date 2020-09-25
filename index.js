851f67c0174d8315872bddf8c543b28c7f1beba7
naina <nanu02d8@gmail.com>
2020-09-25 21:06:09 +0530

    changed code format

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
 document.querySelectorAll('.drum').forEach(drum => drum.addEventListener('click', function () {
     let keyinnerHTML = this.innerHTML;
-    switch (keyinnerHTML) {
-        case "w":
-            let audio1 = new Audio("sounds/tom-1.mp3");
-            audio1.play();
-            break;
-        case "a":
-            let audio2 = new Audio("sounds/tom-2.mp3");
-            audio2.play();
-            break;
-        case "s":
-            let audio3 = new Audio("sounds/tom-3.mp3");
-            audio3.play();
-            break;
-        case "d":
-            let audio4 = new Audio("sounds/tom-4.mp3");
-            audio4.play();
-            break;
-        case "j":
-            let audio5 = new Audio("sounds/snare.mp3");
-            audio5.play();
-            break;
-        case "k":
-            let audio6 = new Audio("sounds/crash.mp3");
-            audio6.play();
-            break;
-        case "l":
-            let audio7 = new Audio("sounds/kick-bass.mp3");
-            audio7.play();
-            break; 
-        default:
-            break;
-    }
+    switchEvents(keyinnerHTML);
 }));
 
 document.addEventListener('keydown', function(event){
-    switch (event.key) {
+    switchEvents(event.key);
+})
+
+function switchEvents(eventName){
+    switch (eventName) {
         case "w":
             let audio1 = new Audio("sounds/tom-1.mp3");
             audio1.play();
@@ -67,4 +40,4 @@ document.addEventListener('keydown', function(event){
         default:
             break;
     }
-})
+}
